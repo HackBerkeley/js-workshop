4a5bbce11c96bf37aa2c40f9b907a518c1b2af4a
Jiahang Li <jiahang_li@outlook.com>
2015-10-08 18:53:10 -0700

    [Tak] improved todo project

--- todo-solution/index.js
+++ todo-solution/index.js
@@ -1,9 +1,11 @@
 function add() {
     // TODO: add the new todo item to the list
-    var content = document.getElementById("content").value;
-    var item = document.createElement("li");
-    var contentNode = document.createTextNode(content);
-    item.appendChild(contentNode);
-    document.getElementById("list").appendChild(item);
+    var content = document.getElementById("content").value.trim();
+    if (content.length > 0) {
+        var listItemElement = document.createElement("li");
+        var textNode = document.createTextNode(content);
+        listItemElement.appendChild(textNode);
+        document.getElementById("list").appendChild(listItemElement);
+    }
     document.getElementById("content").value = "";
 }
